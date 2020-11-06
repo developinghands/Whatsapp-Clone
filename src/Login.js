b4b1a8edab448c70ec162cbafc3d1cb9b2a7ea2c
developinghands <nikhilmishra262@gmail.com>
2020-11-06 23:29:35 +0530

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
 
 export default function Login() {
+  const singIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(result => console.log(result))
+      .catch(error => alert(error));
+  };
   return (
     <div className="login">
       <div className="login_container">
@@ -10,7 +17,7 @@ export default function Login() {
         <div className="login_text">
           <h1>Sing in to WhatsApp</h1>
         </div>
-        <Button>Sign In with Google</Button>
+        <Button onClick={singIn}>Sign In with Google</Button>
       </div>
     </div>
   );
